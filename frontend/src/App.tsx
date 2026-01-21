7a5bbcc05f9f370b566084d77889c4c4effa53a4
dltmthilina <dltmthilinamaduranga@gmail.com>
2026-01-21 15:46:50 +0530

    create initial rect app

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+
+import { Route, Routes } from 'react-router'
+import './App.css'
+import Home from './pages/Home/Home'
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home/>} />
+   </Routes>
+  )
+ }
+  
+
+export default App
